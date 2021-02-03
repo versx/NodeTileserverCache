8b38271ae107cc227b64ab452df43196bb0c2fcb
versx <versx@ver.sx>
2021-02-02 19:33:28 -0800

    Remove redundancies

--- src/extensions/generics.ts
+++ src/extensions/generics.ts
@@ -2,12 +2,12 @@
 
 export class GenericsExtensions {
     public static parse<T>(json: string): T[] {
-        if (!json) return [];
         const list: T[] = [];
-        json = (json || '')?.toString()?.replace(/%22/g, '"');
+        if (!json) return list;
+        json = json.toString()?.replace(/%22/g, '"');
         if (json) {
             return JSON.parse(json);
         }
-        return list || [];
+        return list;
     }
 }
